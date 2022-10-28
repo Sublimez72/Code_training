06987597728ddd59809a36419b5ae31f35f86a57
Sublimez72 <vaggge@hotmail.com>
2022-10-29 01:51:32 +0200

    Added another test

--- numpy_speed_test.py
+++ numpy_speed_test.py
@@ -21,10 +21,17 @@ def built_in_test(num=10_000_000):
     return sum(range(num))
 
 def numpy_test(num=10_000_000):
-    return np.sum(np.arange(num))
+    np.sum(np.arange(num))
+
+def for_iterating_np_array(num=10_000_000):
+    s = 0
+    for s in np.arange(num):
+        s += 1
+    return s
 
 
 print("For loop\t\t", timeit.timeit(for_loop_test, number=1))
 print("While loop\t\t", timeit.timeit(while_loop_test, number=1))
-print("Built in methods\t\t", timeit.timeit(built_in_test, number=1))
-print("Numpy\t\t", timeit.timeit(numpy_test, number=1))
+print("Built in methods\t", timeit.timeit(built_in_test, number=1))
+print("Numpy\t\t\t", timeit.timeit(numpy_test, number=1))
+print("For loop numpy array\t", timeit.timeit(for_iterating_np_array, number=1))
