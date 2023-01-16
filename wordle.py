b435243b0eb7948041f245545086e9376d5f0c03
Vangelis Baimboutzouglou <vaggge@hotmail.com>
2023-01-16 05:56:46 +0100

    added better wordlist

--- wordle.py
+++ wordle.py
@@ -1,25 +1,34 @@
-from english_words import get_english_words_set
 import json
 import os
 from sys import platform
 
 
-path = os.getcwd()
-if platform == "linux" or platform == "linux2":
-    path = path + "/wordle.json"
-elif platform == "darwin":
-    path = path + "/wordle.json"
-elif platform == "win32":
-    path = path + "\wordle.json"
-
-guess = "salet"
-result = "wwwmw"
+guess = "goose"
+result = "wcwcc"
 
 cypher = {}
 for count, letter in enumerate(guess):
     cypher[letter] = result[count]
 
-print(cypher)
+
+def paths(path, save=bool):
+    if save:
+        if platform == "linux" or platform == "linux2":
+            path = path + "/wordle.json"
+        elif platform == "darwin":
+            path = path + "/wordle.json"
+        elif platform == "win32":
+            path = path + "\wordle.json"
+        return path
+
+    if not save:
+        if platform == "linux" or platform == "linux2":
+            path = path + "/words_alpha.txt"
+        elif platform == "darwin":
+            path = path + "/words_alpha.txt"
+        elif platform == "win32":
+            path = path + "\words_alpha.txt"
+        return path
 
 
 def trim(words, cypher):
@@ -28,27 +37,32 @@ def trim(words, cypher):
         if cypher[key] == "w":
             words = [word for word in words if key not in word]
 
-        if cypher[key] == "c":
+        elif cypher[key] == "c":
             words = [word for word in words if key == word[count]]
 
-        if cypher[key] == "m":
+        elif cypher[key] == "m":
             words = [word for word in words if key in word and key != word[count]]
 
     return words
 
 
+path = os.getcwd()
 if "wordle.json" in os.listdir():
+    path = paths(path, True)
     with open(path, "r", encoding="utf-8") as f:
         words = json.load(f)
-else:
-    words = list(get_english_words_set(['web2'], lower=True))
-    words = [s for s in words if len(s) == 5]
 
+else:
+    path = paths(path, False)
+    words = open(path, "r").read().splitlines()
+    words = [s for s in words if len(s) == 5]
 
 words = trim(words, cypher)
 
 print(words)
 print(len(words))
 
+path = os.getcwd()
+path = paths(path, True)
 with open(path, "w", encoding="utf-8") as f:
     json.dump(words, f)
