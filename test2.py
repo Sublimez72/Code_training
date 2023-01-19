8642066c09b2f6cf4c33b88adc84bb02aedb8052
Sublimez72 <vaggge@hotmail.com>
2023-01-19 11:51:25 +0100

    Still broken

--- test2.py
+++ test2.py
@@ -19,11 +19,23 @@
 # gray = w
 
 # change this variable if you're playing a custom wordle game with a different number of letters
-how_many_letters_wordle_game = 4
+how_many_letters_wordle_game = 7
+
+# erasion
+# ygwgwww
+
+# dressed
+# wgggwyw
+
+# correct
+# presume
+
+
+def has_duplicate_letters(word):
+    return len(set(word)) != len(word)
 
 
 def filter_word_list(words, guess, result):
-
     temp_dict = {}
 
     for index, letter in enumerate(guess):
@@ -32,22 +44,23 @@ def filter_word_list(words, guess, result):
         elif letter in temp_dict:
             temp_dict[letter+str(index)] = result[index]
 
-    print(temp_dict)
-
     temp_tuple = tuple(words)
     for word in temp_tuple:
 
         for i in range(how_many_letters_wordle_game):
 
-
             if result[i] == "w" and guess[i] in word:
                 for z in range(how_many_letters_wordle_game):
-                    if guess[i]+str(z) in temp_dict and temp_dict[guess[i]+str(z)] != "w":
+                    if guess[i]+str(z) in temp_dict and temp_dict[guess[i]+str(z)] != "w" or temp_dict[guess[i]] != "w":
                         if word[i] == guess[i]:
+
                             words.remove(word)
+                            break
+                    elif guess[i] + str(z) not in temp_dict:
+
+                        words.remove(word)
                         break
-                else:
-                    words.remove(word)
+
                 break
 
             elif result[i] == "g" and guess[i] != word[i]:
@@ -55,15 +68,14 @@ def filter_word_list(words, guess, result):
                 for z in range(how_many_letters_wordle_game):
                     if guess[i] + str(z) in temp_dict and temp_dict[guess[i]+str(z)] == "g":
                         count += 1
-                    
+
                 if word.count(guess[i]) != count:
                     words.remove(word)
                     break
-                
+
                 elif word.count(guess[i]) == count and guess[i] != result[i]:
                     words.remove(word)
                     break
-
 
             elif result[i] == "y" and guess[i] not in word:
                 words.remove(word)
@@ -73,13 +85,25 @@ def filter_word_list(words, guess, result):
                 words.remove(word)
                 break
 
-    return words
+            elif result[i] == "y":
+                count = 1
+                for z in range(how_many_letters_wordle_game):
+                    if guess[i] + str(z) in temp_dict and temp_dict[guess[i]+str(z)] == "y":
+                        count += 1
 
+                if word.count(guess[i]) < count:
+                    words.remove(word)
+                    break
+
+                elif word.count(guess[i]) == count and guess[i] != result[i]:
+                    words.remove(word)
+                    break
+    return words
 
 # Line 56-81 was written by chatgpt
 
 
-def most_common_letters(words_list):
+def most_common_letters(words_list, dupe):
     # Create an empty dictionary to store the letter counts
     letter_count = {}
     for word in words_list:
@@ -98,6 +122,10 @@ def most_common_letters(words_list):
                 common_letters += letter_count[letter]
         common_letters_list.append((word, common_letters))
 
+    # filter the list that contain duplicate letters
+    if dupe:
+        common_letters_list = [
+            word for word in common_letters_list if not has_duplicate_letters(word[0])]
     # Sort the list of tuples by common letters count in descending order
     common_letters_list.sort(key=lambda x: x[1], reverse=True)
 
@@ -110,7 +138,7 @@ words = [s for s in words if len(s) == how_many_letters_wordle_game]
 
 
 print(f"Size of wordlist: {len(words)}")
-print(f"Possible first guesses:  {most_common_letters(words)}")
+print(f"Possible first guesses:  {most_common_letters(words, True)}")
 
 while True:
     guess = input("Guess: ").lower()
@@ -123,10 +151,12 @@ while True:
     words = filter_word_list(words, guess, result)
     print(words)
     print()
-    next_guess = most_common_letters(words)
+    next_guess = most_common_letters(words, True)
 
     if len(next_guess) == 0:
-        print("No recommended guesses!")
+        print("No good guesses!")
+        print("Try these instead")
+        print(most_common_letters(words, False))
     else:
         print(next_guess)
     print(len(words))
