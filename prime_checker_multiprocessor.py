88716461d0247b10cc7b94997453c77aa6e28c42
Sublimez72 <vaggge@hotmail.com>
2023-01-30 10:47:08 +0100

    Optimized the factor_finder function

--- prime_checker_multiprocessor.py
+++ prime_checker_multiprocessor.py
@@ -10,7 +10,13 @@ num = 19999999800000001
 
 
 def factor_finder(i):
-    if num % i == 0:
+    if num == 2:
+        return True
+
+    elif num % 2 == 0:
+        return False
+    
+    elif num % i == 0:
             return i
     else:
         return False
@@ -21,7 +27,7 @@ if __name__ == '__main__':
 
     with multiprocessing.Pool() as p:
         start_t = time.perf_counter()
-        for i in p.map_async(factor_finder, range(2, ceil(isqrt(num)))).get():
+        for i in p.map_async(factor_finder, range(3, ceil(isqrt(num)), 2)).get():
             if type(i) == int:
                 end_t = time.perf_counter()
                 print(f"{num} is NOT a prime!\nDuration of search: {end_t - start_t}")
